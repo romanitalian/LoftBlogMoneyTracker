acd6af67e380a275380f46633c75714b83a9d825
Constantine Chernenko <constantine.chernenko@gmail.com>
2015-08-28 11:42:28 +0300

    Toolbar was added

--- app/src/main/java/com/loftschool/loftmoneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/loftmoneytracker/MainActivity.java
@@ -1,22 +1,34 @@
 package com.loftschool.loftmoneytracker;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initToolbar();
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new Expenses())
                     .commit();
         }
+    }
+
+    private void initToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
 }
